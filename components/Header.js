ad4ef379e630873be64b5df78cabc2a89f65f62c
Kenneth <k.owusu.me@gmail.com>
2022-01-01 12:51:43 +0000

    customize tailwindcss and use sass

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return ( 
+        <div className="header">
+            <div className="searchbox">
+                <div className="searchbox__imgcont"><img src="" alt="" className="searchbox__img" /></div>
+                <input type="text" placeholder="search my vault" className="searchbox__textbox" />
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
