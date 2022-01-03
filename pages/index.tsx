1ff2c44d14d83f51d703f612c7c02ce397d9eb47
Kenneth <k.owusu.me@gmail.com>
2022-01-03 16:43:35 +0000

    add mobileview passwords

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,14 @@ import Image from 'next/image'
 import Header from '../components/Header';
 import MenuSidebar from '../components/MenuSidebar';
 import Sort from '../components/Sort';
+import Password from '../components/Passoword';
 
 const Home: NextPage = () => {
   return (
     <div className="page_container grid" >
       <Header/>
       <Sort/>
+      <Password/>
       <MenuSidebar/>
     </div>
   )
