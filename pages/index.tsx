21da31162061302629cbce97404e944747b68076
Kenneth <k.owusu.me@gmail.com>
2022-01-03 14:20:46 +0000

    add mobileview sort

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 
+
+
 import Header from '../components/Header';
-import MenuSidebar from '../components/MenuSidebar'
+import MenuSidebar from '../components/MenuSidebar';
+import Sort from '../components/Sort';
 
 const Home: NextPage = () => {
   return (
     <div className="page_container grid" >
       <Header/>
+      <Sort/>
       <MenuSidebar/>
     </div>
   )
