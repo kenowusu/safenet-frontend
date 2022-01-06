b7b80be15a3448d6176431fdf8515ce979e5bc5c
Kenneth <k.owusu.me@gmail.com>
2022-01-06 21:35:15 +0000

    lgview: password

--- components/Passoword.js
+++ components/Passoword.js
@@ -169,6 +169,35 @@ const Password = () => {
                 </div>
             </div>
 
+            
+            {/* password */}
+            <div className="password mb-4">
+                {/* password__img */}
+                <div className="password__iconcont">
+                    <div className="password__iconcont">
+                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
+                    </div>
+                </div>
+                {/* password__desc */}
+                <div className="password__desc">
+                    <p className="password__desc__name">Google</p>
+                    <div className="password__desc__username">someone@gmail.com</div>
+                    <div className="password__desc__password">............</div>
+                </div>
+                
+                {/* password__options */}
+                <div className="password__options">
+                    <button className="password__options__ellipse">
+                        <EllipseSvg/>
+                    </button>
+                    <button className="password__options__reveal">
+                        <PasswordShowSvg/>
+                    </button>
+                </div>
+            </div>
+
+
+
 
 
 
