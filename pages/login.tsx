f8ade4dd46aa869b2545face18d39e0d6d26c74e
Kenneth <k.owusu.me@gmail.com>
2022-02-12 18:59:03 +0000

    login user

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,122 @@
+import {useState,useEffect} from 'react';
 import Link from 'next/link';
 import LogoImage from '../public/icons/logo.svg';
+import isLoggedIn from '../lib/user/isLoggedIn';
+import {userVal} from '../validations/userValidation';
+import Cookies from 'universal-cookie';
 
-const LoginPage = ()=>{
+
+
+
+export const getServerSideProps = async(context)=>{
+    
+    const API = process.env.API;
+
+     const isloggedin = await isLoggedIn(API,context);
+
+     if(isloggedin){
+        return{
+            redirect:{
+                destination:"/dashboard"
+            }
+        }
+     }
+
+    return{
+        props:{
+            url:API
+        }
+    }
+
+
+}
+
+
+
+
+
+
+const LoginPage = (props)=>{
+
+
+      //set states
+      const [email,setEmail] = useState('');
+      const [password,setPassword] = useState('');
+      const [counter,setCounter] = useState(0);
+      const [token,setToken] = useState('');
+      const [valError,setValError] = useState({
+                                              validationError:"",
+                                              style:{display:"none"}                                           
+                                          })
+  
+      
+      const user = {email,password};
+
+          //use Effects
+    useEffect(async()=>{
+       
+        setValError({validationError:"",style:{display:'none'}})
+        try{
+            const validateUser =  await userVal.validateSync(user);
+          }catch(err){
+            let formerr  = err.errors[0];
+            if(counter >=1){
+                setValError({validationError:formerr,style:{display:'inline-block'}})
+            }
+            setCounter(counter+1);
+          }
+    },[email,password]);
+
+
+
+
+    //register user
+const loginUser = async(e)=>{ 
+    e.preventDefault();
+   
+    const isValid = await userVal.isValid(user);
+    
+    if(!isValid){return}
+   
+
+      const url = props.url;
+      const api = `${url}/api/users/login`;
+      const body =  JSON.stringify({email,password});
+     
+      
+      let reguser =  await fetch(api,{
+           method:"POST",
+          credentials:'include',
+           body:body,
+           headers:{'Content-Type':"application/json"}
+      })
+
+      
+      if(reguser.status== 400 || reguser.status == 500||  reguser.status==600){
+          let formerr = "Server error,please try again";
+          setValError({validationError:formerr,style:{display:'inline-block'}})
+      }else if(reguser.status==401){
+          let response = await reguser.json();
+          let formerr = response.message;
+          setValError({validationError:formerr,style:{display:'inline-block'}})
+      }
+
+      else if(reguser.status == 200){
+          let response = await reguser.json();
+          const token = response.token;
+          const cookies = new Cookies();
+        
+          const setcookie = await cookies.set('tk',token);
+          window.location.href = "/dashboard";
+          
+      }
+     
+  }
+
+      
 
     return(
-        <form className="inup">
+        <form className="inup" onSubmit={loginUser}>
             <Link href="/">
                <a href=""><div className="inup__logo"><LogoImage/></div></a>
             </Link>
@@ -14,17 +126,20 @@ const LoginPage = ()=>{
                 <Link href="/signup"><a className="inup__textlink__link">Sign Up</a></Link>
             </div>
 
-            <span className="inup__validationError"></span>
+            <span style={valError.style} className="inup__validationError">{valError.validationError}</span>
 
             <div className="inup__itemcont">
                 <div className="inup__item">
                     <label className="inup__item__label" htmlFor="">Email address</label>
-                    <input  type="text" className="inup__item__textbox" />
+                    <input  type="text" className="inup__item__textbox" 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="inup__item">
                     <label  className="inup__item__label" htmlFor="">Password</label>
-                    <input type="password" className="inup__item__textbox" />
+                    <input type="password" className="inup__item__textbox"
+                    onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
 
                 <div className="inup__submit">
