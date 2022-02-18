0e9f5bb4e5c1505f3a59ec903ecf5ac8c12cf462
Kenneth <k.owusu.me@gmail.com>
2022-02-18 11:38:13 +0000

    redesign password mobile view

--- components/Passoword.js
+++ components/Passoword.js
@@ -1,6 +1,7 @@
 
-import EllipseSvg from '../public/icons/ellipses-horizontal.svg';
-import PasswordShowSvg from '../public/icons/show-password.svg';
+import EllipseSvg from '../public/icons/options-vertical.svg';
+import PasswordShowSvg from '../public/icons/eye-reveal.svg';
+import PasswordCopySvg from '../public/icons/copy.svg';
 
 
 const Password = () => {
@@ -18,89 +19,14 @@ const Password = () => {
                 {/* password__desc */}
                 <div className="password__desc">
                     <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
-                </div>
-                
-                {/* password__options */}
-                <div className="password__options">
-                    <button className="password__options__ellipse">
-                        <EllipseSvg/>
-                    </button>
-                    <button className="password__options__reveal">
-                        <PasswordShowSvg/>
-                    </button>
-                </div>
-            </div>
-
-
-             {/* password */}
-             <div className="password mb-4">
-                {/* password__img */}
-                <div className="password__iconcont">
-                    <div className="password__iconcont">
-                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
                     </div>
-                </div>
-                {/* password__desc */}
-                <div className="password__desc">
-                    <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
-                </div>
-                
-                {/* password__options */}
-                <div className="password__options">
-                    <button className="password__options__ellipse">
-                        <EllipseSvg/>
-                    </button>
-                    <button className="password__options__reveal">
-                        <PasswordShowSvg/>
-                    </button>
-                </div>
-            </div>
-
-
-             {/* password */}
-             <div className="password mb-4">
-                {/* password__img */}
-                <div className="password__iconcont">
-                    <div className="password__iconcont">
-                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
                     </div>
-                </div>
-                {/* password__desc */}
-                <div className="password__desc">
-                    <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
-                </div>
-                
-                {/* password__options */}
-                <div className="password__options">
-                    <button className="password__options__ellipse">
-                        <EllipseSvg/>
-                    </button>
-                    <button className="password__options__reveal">
-                        <PasswordShowSvg/>
-                    </button>
-                </div>
-            </div>
-
-            
-             {/* password */}
-             <div className="password mb-4">
-                {/* password__img */}
-                <div className="password__iconcont">
-                    <div className="password__iconcont">
-                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
-                    </div>
-                </div>
-                {/* password__desc */}
-                <div className="password__desc">
-                    <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
                 </div>
                 
                 {/* password__options */}
@@ -126,8 +52,14 @@ const Password = () => {
                 {/* password__desc */}
                 <div className="password__desc">
                     <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
+                    </div>
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
+                    </div>
                 </div>
                 
                 {/* password__options */}
@@ -140,7 +72,6 @@ const Password = () => {
                     </button>
                 </div>
             </div>
-
 
 
             {/* password */}
@@ -154,8 +85,14 @@ const Password = () => {
                 {/* password__desc */}
                 <div className="password__desc">
                     <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
+                    </div>
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
+                    </div>
                 </div>
                 
                 {/* password__options */}
@@ -169,7 +106,7 @@ const Password = () => {
                 </div>
             </div>
 
-            
+
             {/* password */}
             <div className="password mb-4">
                 {/* password__img */}
@@ -181,8 +118,14 @@ const Password = () => {
                 {/* password__desc */}
                 <div className="password__desc">
                     <p className="password__desc__name">Google</p>
-                    <div className="password__desc__username">someone@gmail.com</div>
-                    <div className="password__desc__password">............</div>
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
+                    </div>
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
+                    </div>
                 </div>
                 
                 {/* password__options */}
@@ -196,6 +139,71 @@ const Password = () => {
                 </div>
             </div>
 
+
+            {/* password */}
+            <div className="password mb-4">
+                {/* password__img */}
+                <div className="password__iconcont">
+                    <div className="password__iconcont">
+                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
+                    </div>
+                </div>
+                {/* password__desc */}
+                <div className="password__desc">
+                    <p className="password__desc__name">Google</p>
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
+                    </div>
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
+                    </div>
+                </div>
+                
+                {/* password__options */}
+                <div className="password__options">
+                    <button className="password__options__ellipse">
+                        <EllipseSvg/>
+                    </button>
+                    <button className="password__options__reveal">
+                        <PasswordShowSvg/>
+                    </button>
+                </div>
+            </div>
+
+
+            {/* password */}
+            <div className="password mb-4">
+                {/* password__img */}
+                <div className="password__iconcont">
+                    <div className="password__iconcont">
+                        <img className="password__icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"  alt="" />
+                    </div>
+                </div>
+                {/* password__desc */}
+                <div className="password__desc">
+                    <p className="password__desc__name">Google</p>
+                    <div className="password__desc__username">
+                        <span className="password__desc__username__username">someone@gmail.com</span>
+                        <button className="password__desc__username__copy"><PasswordCopySvg/></button>
+                    </div>
+                    <div className="password__desc__password">
+                        <span className="password__desc__password__password">.........................</span>
+                        <button className="password__desc__password__copy"><PasswordCopySvg/></button>
+                    </div>
+                </div>
+                
+                {/* password__options */}
+                <div className="password__options">
+                    <button className="password__options__ellipse">
+                        <EllipseSvg/>
+                    </button>
+                    <button className="password__options__reveal">
+                        <PasswordShowSvg/>
+                    </button>
+                </div>
+            </div>
 
 
 
