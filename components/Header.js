08bee62e8a3bab5768811686c9633bd50b38bc06
Kenneth <k.owusu.me@gmail.com>
2022-02-18 14:50:10 +0000

    change layout

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import SearchSvg from '../public/icons/search.svg';
 const Header = () => {
     return ( 
         <div className="header">
-            <div className="searchbox  flex h-8 w-full rounded-md">
+            <div className="searchbox  flex  w-full rounded-md">
                 <div className="searchbox__imgcont flex w-7 p-0.5"><SearchSvg/></div>
                 <input type="text"  className="searchbox__textbox w-full h-full rounded-md p-2"  placeholder="search my vault"/>
             </div>
