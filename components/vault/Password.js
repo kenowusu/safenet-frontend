943ffa47419e0faaaa9fedb93d6f61a53c7c9fd0
Kenneth <k.owusu.me@gmail.com>
2022-02-21 18:53:18 +0000

    password markup

--- components/vault/Password.js
+++ components/vault/Password.js
@@ -10,7 +10,54 @@ import PasswordCopySvg from '../../public/icons/copy.svg';
 const Password = () => {
     return ( 
       
-        <div>
+        <div className="password">
+
+            <div className="password-container">
+
+
+                <table className="password-table">
+
+                    <thead className="password-table-tbh">
+                        <tr className="password-table-tbh-tr">
+                            <td className='password-table-tbh-tr-td name'>Name</td>
+                            <td className='password-table-tbh-tr-td folder'>Folder</td>
+                            <td className='password-table-tbh-tr-td options'></td>
+                        </tr>
+                    </thead>
+
+                    <tr className="password-item-tr">
+                        <td className="password-item-td-name">
+                            <span className="password-item-td-name-name">AWS AMAZON</span>
+                            <span className='password-item-td-username'>kennethowusu@gmail.com</span>
+                        </td>
+                        <td className="password-item-td-folder">email</td>
+                        <td className="password-item-td-options">...</td>
+                    </tr>
+
+
+                    <tr className="password-item-tr">
+                        <td className="password-item-td-name">
+                            <span className="password-item-td-name-name">Amazon</span>
+                            <span className='password-item-td-username'>kennethowusu@gmail.com</span>
+                        </td>
+                        <td className="password-item-td-folder">courses</td>
+                        <td className="password-item-td-options">...</td>
+                    </tr>
+
+
+                    <tr className="password-item-tr">
+                        <td className="password-item-td-name">
+                            <span className="password-item-td-name-name">Cybrary</span>
+                            <span className='password-item-td-username'>christy4153</span>
+                        </td>
+                        <td className="password-item-td-folder">Google</td>
+                        <td className="password-item-td-options">...</td>
+                    </tr>
+
+                </table>
+              
+            </div>{/*password-container*/}
+
             
         </div>
      );
