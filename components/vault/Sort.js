f7a0838b336b025f2e419ce33cecee7b69e81a8b
Kenneth <k.owusu.me@gmail.com>
2022-02-21 15:31:59 +0000

    sort markup

--- components/vault/Sort.js
+++ components/vault/Sort.js
@@ -1,12 +1,26 @@
 
 //==============svg images ====================//
 
-import SortSvg from '../../public/icons/filter.svg';
+import SortSvg from '../../public/icons/options/options-sort.svg';
 
 const Sort = () => {
     return ( 
-        <div>
-            
+        <div className="sort">
+
+            <div className="sort-container flex  items-center">
+
+              <p className="sort-text">Passwords</p>
+
+              <div className="sort-option flex justify-end items-center">
+                  <button className="sort-option-imgc flex justify-center items-center">
+                      <SortSvg/>
+                      <p className="sort-option-text">Sort</p>
+                   </button>
+                  
+              </div>{/*sort option*/}
+
+            </div>{/*sort-container */}
+
         </div>
      );
 }
