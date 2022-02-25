dc6cb740ccfc8e8839f2c345b1530f57429a0674
Kenneth <k.owusu.me@gmail.com>
2022-02-25 16:19:55 +0000

    logout user

--- components/dropdown/accountDropDown.js
+++ components/dropdown/accountDropDown.js
@@ -1,4 +1,6 @@
-import {useEffect} from 'react'
+import {useEffect} from 'react';
+import Cookies from 'universal-cookie';
+import {Router, useRouter} from 'next/router';
 
 //============import svg images ================//
 import FolderSvgImage from '../../public/icons/options/options-folder.svg';
@@ -8,25 +10,38 @@ import RecentSvgImage from '../../public/icons/options/options-recent.svg';
 
 
 const AccountDropDown = () => {
+ 
 
+
+    // useEffect(()=>{
+    //     const sortShowItems = document.querySelectorAll('.dropdown-option-item__hide');
+    //     const skipBtnForElements = document.querySelectorAll('.dropdown-title');
+   
+    //     skipBtnForElements.forEach((item)=>{
+    //        item.addEventListener('click',(e)=>{
+    //         //    e.stopPropagation();
+    //        })
+    //     })
+    //     sortShowItems.forEach((sortShowItem)=>{
+    //         sortShowItem.addEventListener('click',(e)=>{
+    //             // e.stopPropagation();
+    //             console.log('ok')
+    //         })
+    //     })
+    //    },[])
     
+    //===============logout =============//
 
-    useEffect(()=>{
-     const sortShowItems = document.querySelectorAll('.dropdown-option-item__hide');
-     const skipBtnForElements = document.querySelectorAll('.dropdown-title');
+    const logout = ()=>{
+       
+        const cookies = new Cookies();
+        //clear cookie and redirect to '/login'
+        cookies.set('tk','');
+        window.location.href = '/login';
+        
+    }
 
-     skipBtnForElements.forEach((item)=>{
-        item.addEventListener('click',(e)=>{
-            e.stopPropagation();
-        })
-     })
-     sortShowItems.forEach((sortShowItem)=>{
-         sortShowItem.addEventListener('click',(e)=>{
-             e.stopPropagation();
-             console.log('ok')
-         })
-     })
-    },[])
+
    
     return ( 
         <div className="dropdown  account-dropdown absolute ">
@@ -36,7 +51,7 @@ const AccountDropDown = () => {
 
                 <div className="dropdown-option">
                     <div className="dropdown-option-item"><span className="dropdown-option-item-text">Settings</span></div>
-                    <div className="dropdown-option-item"><span className="dropdown-option-item-text">Logout</span></div>
+                    <div className="dropdown-option-item" onClick={logout}><span className="dropdown-option-item-text">Logout</span></div>
                 
                 </div>{/*dropdown-option */}
             
--- components/vault/Header.js
+++ components/vault/Header.js
@@ -12,34 +12,34 @@ import UserSvg from '/public/icons/options/options-user.svg';
 import AccountDropDown from '../dropdown/accountDropDown';
 
 const Header = () => {
-    
-    useEffect(()=>{
-        const sortOptionBtn = document.querySelector('.header-options-userc-btn');
-        sortOptionBtn.addEventListener('click',e=>{
+
+    // useEffect(()=>{
+    //     const sortOptionBtn = document.querySelector('.header-options-userc-btn');
+    //     sortOptionBtn.addEventListener('click',e=>{
          
-            e.stopPropagation();
+    //         e.stopPropagation();
             
-            const openedDropdowns = document.querySelectorAll('.dropdown__show');
-            /*
-             Hide all opened dropdown and open/show current clicked dropdown
-             Visit this again
-            */
-            openedDropdowns.forEach(openedDropdown=>{
-                if(openedDropdown !== e.target){
-                   openedDropdown.classList.remove('dropdown__show')
-                }
+    //         const openedDropdowns = document.querySelectorAll('.dropdown__show');
+    //         /*
+    //          Hide all opened dropdown and open/show current clicked dropdown
+    //          Visit this again
+    //         */
+    //         openedDropdowns.forEach(openedDropdown=>{
+    //             if(openedDropdown !== e.target){
+    //                openedDropdown.classList.remove('dropdown__show')
+    //             }
                 
-            })
-            sortOptionBtn.classList.toggle('dropdown__show')
-        })
+    //         })
+    //         sortOptionBtn.classList.toggle('dropdown__show')
+    //     })
            
-    },[]);  
+    // },[]);  
     
     return ( 
         <div className="header">
 
              <div className="header-container">
-
+              
                  <div className="header-searchbox">
                      <input type="text" className="header-searchbox-textbox" name="search" />
                      <label className="header-searchbox-imgc flex justify-center items-center" htmlFor="search" ><SearchSvg/></label>
@@ -48,7 +48,7 @@ const Header = () => {
                  <div className="header-options">
                      <div className="header-options-container">
                          <button className="header-options-importc flex justify-center items-center"><ImportSvg/></button>
-                         <button className="header-options-userc header-options-userc-btn flex justify-center items-center relative">
+                         <button className="header-options-userc header-options-userc-btn dropdown__show flex justify-center items-center relative">
                           <UserSvg/>
                           <AccountDropDown/>
                          </button>
