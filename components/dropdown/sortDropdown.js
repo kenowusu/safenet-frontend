a2b443706b3f0dce8df68cc1b8b27934c4fc24a6
Kenneth <k.owusu.me@gmail.com>
2022-03-07 16:28:53 +0000

    filter passwords by most recent

--- components/dropdown/sortDropdown.js
+++ components/dropdown/sortDropdown.js
@@ -1,4 +1,9 @@
-import {useEffect} from 'react'
+import {useEffect,useContext} from 'react'
+
+
+import sortArray from 'sort-array';
+
+import {PasswordContext} from '../../contexts/PasswordContext';
 
 //============import svg images ================//
 import FolderSvgImage from '../../public/icons/options/options-folder.svg';
@@ -8,8 +13,23 @@ import RecentSvgImage from '../../public/icons/options/options-recent.svg';
 
 
 const SortDropDown = () => {
+   
+    const {passwords,setPasswords} = useContext(PasswordContext);
 
 
+
+   //============filter by most recents==============
+   const getMostRecentPasswords = ()=>{
+    const cpPasswords = [...passwords];
+    const sortByMostRecent = sortArray(cpPasswords,{
+        by:'created_at',
+        order:'asc'
+    })
+    setPasswords(sortByMostRecent);
+
+   
+
+   }
 
 
 
@@ -21,11 +41,12 @@ const SortDropDown = () => {
                 <div className="dropdown-title"><span>Sort By</span></div>
 
                 <div className="dropdown-option">
+                    
                     <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder (a-z)</span></div>
                     <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder(z-a)</span></div>
                     <div className="dropdown-option-item dropdown-option-item__hide"><span className="dropdown-option-item-text">Name(a-z)</span></div>
                     <div className="dropdown-option-item dropdown-option-item__show"><span className="dropdown-option-item-text">Name(z-a)</span></div>
-                    <div className="dropdown-option-item"><span className="dropdown-option-item-text">Most Recent</span></div>
+                    <div onClick={getMostRecentPasswords} className="dropdown-option-item"><span className="dropdown-option-item-text">Most Recent</span></div>
 
                 </div>{/*dropdown-option */}
             
