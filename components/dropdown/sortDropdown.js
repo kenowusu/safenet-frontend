36b6d4098aa425de9bdde652aec5610dd19f5219
Kenneth <k.owusu.me@gmail.com>
2022-03-07 16:40:13 +0000

    filter by password name

--- components/dropdown/sortDropdown.js
+++ components/dropdown/sortDropdown.js
@@ -19,16 +19,31 @@ const SortDropDown = () => {
 
 
    //============filter by most recents==============
-   const getMostRecentPasswords = ()=>{
+   const filterByMostRecent = ()=>{
     const cpPasswords = [...passwords];
     const sortByMostRecent = sortArray(cpPasswords,{
         by:'created_at',
-        order:'asc'
+        order:'desc'
     })
     setPasswords(sortByMostRecent);
+   }
 
-   
+   const filterByNameAz = ()=>{
+    const cpPasswords = [...passwords];
+    const sortByNameAz = sortArray(cpPasswords,{
+        by:'name',
+        order:'asc'
+    })
+    setPasswords(sortByNameAz);
+   }
 
+   const filterByNameZa = ()=>{
+    const cpPasswords = [...passwords];
+    const sortByNameZa = sortArray(cpPasswords,{
+        by:'name',
+        order:'desc'
+    })
+    setPasswords(sortByNameZa);
    }
 
 
@@ -42,11 +57,11 @@ const SortDropDown = () => {
 
                 <div className="dropdown-option">
                     
-                    <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder (a-z)</span></div>
-                    <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder(z-a)</span></div>
-                    <div className="dropdown-option-item dropdown-option-item__hide"><span className="dropdown-option-item-text">Name(a-z)</span></div>
-                    <div className="dropdown-option-item dropdown-option-item__show"><span className="dropdown-option-item-text">Name(z-a)</span></div>
-                    <div onClick={getMostRecentPasswords} className="dropdown-option-item"><span className="dropdown-option-item-text">Most Recent</span></div>
+                    {/* <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder (a-z)</span></div>
+                    <div className="dropdown-option-item"><span className="dropdown-option-item-text">Folder(z-a)</span></div> */}
+                    <div onClick={filterByNameAz} className="dropdown-option-item dropdown-option-item__hide"><span className="dropdown-option-item-text">Name(a-z)</span></div>
+                    <div onClick={filterByNameZa} className="dropdown-option-item dropdown-option-item__show"><span className="dropdown-option-item-text">Name(z-a)</span></div>
+                    <div onClick={filterByMostRecent} className="dropdown-option-item"><span className="dropdown-option-item-text">Most Recent</span></div>
 
                 </div>{/*dropdown-option */}
             
